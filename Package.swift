04537f70370f8a9ba131cc7b0f0a8025a2f5698c
Scott Lucas <scott@scotts-macbook.local>
2018-05-29 16:49:19 -0700

    package dependancies

--- Package.swift
+++ Package.swift
@@ -4,12 +4,9 @@ import PackageDescription
 let package = Package(
     name: "Toasty",
     dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.3"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.2.3"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "2.0.2")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "2.0.0")
     ],
     
     targets: [
