aa95093e1f8bb175337dc4985b70601229aa0f14
Scott Lucas <scott@scotts-macbook.local>
2018-05-29 17:08:26 -0700

    dep

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "Toasty",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "2.0.0")
     ],
     
