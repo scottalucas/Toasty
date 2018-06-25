a439d826e75bd3682b0d66ce09a1ec45e772ec80
scottalucas <scott@slucas.com>
2018-06-25 14:20:01 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,13 +7,11 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc")
-//        .package(url: "./JSONValidator", from: "1.0.0")
     ],
     
     targets: [
         .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor", "Leaf"]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
+        .target(name: "Run", dependencies: ["App"])
     ]
 )
 
