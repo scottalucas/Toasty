a1d522e102b361026d27227e86c95d9fc6cdeab8
Scott Lucas <scott@slucas.com>
2021-03-21 15:27:37 -0700

    add log

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,6 +9,7 @@ public func configure(
 	_ env: inout Environment,
 	_ services: inout Services
 	) throws {
+    print ("Starting config...")
 	try services.register(FluentPostgreSQLProvider())
 	let router = EngineRouter.default()
 	try routes(router)
